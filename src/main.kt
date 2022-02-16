19c73a332c21ce199b3be9b6964683c2748a3617
caro-mugo <mugonycaroline@gmail.com>
2022-02-16 21:09:04 +0300

    kotlin assignment

---
+++ src/main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var studentName:String="caroline nyambura mugo"
+    println(studentName)
+    var age:Int=22
+    println(age)
+    var phoneNumber:String="0729284253"
+    println(phoneNumber)
+    var weight=76
+    println(weight)
+    val kenyan:Boolean=true
+    println(kenyan)
+}
